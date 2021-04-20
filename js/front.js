028a49bfb84f8f7c8eb1d1ba32bed6e4134a35e9
Cesar Vilca <vilcagamarracf@gmail.com>
2021-04-20 04:21:09 -0500

    :rocket: Big Update
    Remodelación!

--- js/front.js
+++ js/front.js
@@ -1,23 +1,59 @@
 
 // $('body').scrollspy({ target: '#myScrollspy' })
+// document.querySelector('.menu-btn').addEventListener('click', () => {
+//     document.querySelector('.nav-menu').classList.toggle('show');
+// })
 
-$(document).ready(function () {
 
-    $("#sidebar").mCustomScrollbar({
-         theme: "minimal"
+// BOTON -----------------------------------
+
+    // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
+
+// FIN DE BOTON ----------------------------------
+
+
+//  SIDEBAR ---------------------------------
+
+    $(document).ready(function () {
+
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal"
+        });
+
+        $('#sidebarCollapse').on('click', function () {
+            // open or close navbar
+            $('#sidebar').toggleClass('active');
+            // close dropdowns
+            $('.collapse.in').toggleClass('in');
+            // and also adjust aria-expanded attributes we use for the open/closed arrows
+            // in our CSS
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
+
     });
 
-    $('#sidebarCollapse').on('click', function () {
-        // open or close navbar
-        $('#sidebar').toggleClass('active');
-        // close dropdowns
-        $('.collapse.in').toggleClass('in');
-        // and also adjust aria-expanded attributes we use for the open/closed arrows
-        // in our CSS
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
+// FIN DE SIDEBAR --------------------------------
 
-});
 
 /* =========================================
  *  map
