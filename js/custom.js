2c8ca5010c4a721429d7e50a7b59adb029cdf44d
Cesar Vilca <vilcagamarracf@gmail.com>
2021-05-07 19:16:53 -0500

    :rocket: Update
    Mejora!

--- js/custom.js
+++ js/custom.js
@@ -18,8 +18,8 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 // FIN DE BOTON ----------------------------------
